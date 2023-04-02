6191315a026a52d24f1be5b82effd0b79eb74813
Akash Dhar <akshdhr117@gmail.com>
2023-04-02 16:58:49 +0530

    Adding Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,31 @@
+import { Stack, Box, Typography } from '@mui/material'
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{
+      flexDirection: { sx:"column", md:"row" } 
+    }}>
+      <Box sx={{
+        height:{ sx:"auto", md:"92vh" },
+        borderRight: '1px solid #3d3d3d',
+        px: { sx:0, md:2 }
+      }}>
+        <Sidebar />
+        <Typography 
+          className="copyright"
+          variant="body2"
+          sx = {{
+            mt:1.5,
+            color: '#fff'
+          }}
+        >
+          Copyright 2022 YouTube 2.0
+        </Typography>
+      </Box>
+
+    </Stack>
   )
 }
 
