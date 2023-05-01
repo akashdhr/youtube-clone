028ea226f551917f4718d13cbac17e30ee5caca1
Akash Dhar <akshdhr117@gmail.com>
2023-05-01 20:39:52 +0530

    Setting up API

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,12 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+
 const options = {
   method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/captions',
+  url: BASE_URL,
   params: {
     part: 'snippet',
     videoId: 'M7FIvfx5J10'
   },
   headers: {
-    'X-RapidAPI-Key': '741b3e4962mshf478c1c3af6b07ep11c51fjsna09706c544ea',
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
