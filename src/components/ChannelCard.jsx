921a2b7024adbd966ae1085c1d7d389e274e3e55
Akash Dhar <akshdhr117@gmail.com>
2023-09-21 11:40:16 +0530

    Updated Feed Cards

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = () => {
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>ChannelCard</div>
+    <Box
+        sx={{
+            boxShadow: 'none',
+            borderRadius: 20,
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+            width:{xs: '358px', md:'320px'}
+        }}
+    >
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+            <CardContent sx={{display:'flex', flexDirection:'column', justifyContent:'center', textAlign:'center', color:'#fff'}}>
+                <CardMedia
+                    image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                    alt={channelDetail?.snippet?.title}
+                    sx={{
+                        borderRadius:'50%',
+                        height: 180,
+                        width: 180,
+                        mb: 2,
+                        border: '1px solid #e3e3e3'
+                    }}
+                />
+                <Typography variant="h6"> 
+                    {channelDetail?.snippet?.title}
+                    <CheckCircle sx={{ fontSize: 14, color:'gray', ml: '5px' }}/>
+                { channelDetail?.statistics?.subscriberCount && (
+                    <Typography>
+                        {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+                    </Typography>
+                )}
+                </Typography>
+            </CardContent>
+        </Link>
+    </Box>
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,7 +7,12 @@ const Videos = ({ videos, direction }) => {
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       
-
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
     </Stack>
   );
 }
