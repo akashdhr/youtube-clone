14ae5c6573a5242c150da4ef03f979fc1747288e
Akash Dhar <akshdhr117@gmail.com>
2023-09-24 11:13:18 +0530

    Updated Channel & Video cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
-import { Navbar, Feed, VideoDetail, Channel, SearchFeed }  from './components'
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed }  from './components'
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" exact element={<Feed/>}/>
           <Route path="/video/:id" element={<VideoDetail/>}/>
-          <Route path="/channel/:id" element={<Channel/>}/>
+          <Route path="/channel/:id" element={<ChannelDetail/>}/>
           <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
         </Routes>
       </Box> 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,8 +5,9 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
+
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
-  <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px" }, boxShadow: "none", borderRadius: 0 }}>
+  <Card sx={{ width: {xs: '100%',  sm: '358px', md: "320px" }, boxShadow: "none", borderRadius: 0 }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
         sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
@@ -15,7 +16,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px'}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          {snippet?.title.slice(0,60) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
       <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
