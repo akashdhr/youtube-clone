d7a4e795d3a85fe94dce14d34cfa19e015dab9c4
Akash Dhar <akshdhr117@gmail.com>
2024-02-05 00:57:21 +0530

    Fix Video Detail API

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,7 +9,7 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
-  const [videos, setVideos] = useState(null);
+  const [videos, setVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const VideoDetail = () => {
       .then((data) => setVideoDetail(data.items[0]))
     
     fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
-      .then((data) => setVideos(data.items[0]))
+      .then((data) => setVideos(data.items))
   }, [id]);
 
   if(!videoDetail?.snippet) return <Loader />;
@@ -28,7 +28,7 @@ const VideoDetail = () => {
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "100%", position: "sticky"}}>
+          <Box sx={{ width: "100%", position: "sticky", top:"86px"}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
@@ -51,10 +51,10 @@ const VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
+        <Box px={2} py={{md: 1, xs: 5}} justifyContent="center" alignItems="center">
+          <Videos videos={videos} direction="column" />
+        </Box>
       </Stack>
-      <Box px={2} py={{md: 1, xs: 5}} justifyContent="center" alignItems="center">
-        <Videos videos={videos} />
-      </Box>
     </Box>
   );
 };
