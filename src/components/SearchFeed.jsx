f133fa22adb41cdbc951804279601ccba799ac40
Akash Dhar <akshdhr117@gmail.com>
2024-02-05 00:44:43 +0530

    Fix Search and Video Details

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+    }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2, ml: {md:'100px'}}}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search results for:  <span style={{ color: "#FC1503" }}>{ searchTerm }</span> videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
